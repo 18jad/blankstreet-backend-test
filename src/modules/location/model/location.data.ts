fd12d8f231800e70667395b418053ed292fe010a
Jad Yahya <jadoyahya@gmail.com>
2025-08-09 16:51:55 +0300

    feat: add tax rate property to LocationData model

--- src/modules/location/model/location.data.ts
+++ src/modules/location/model/location.data.ts
@@ -14,10 +14,16 @@ export class LocationData {
     @ApiProperty({ description: "Currency code (ISO 4217)" })
     public readonly currencyCode: string;
 
+    @ApiProperty({
+        description: "Tax rate in basis points (e.g., 1000 = 10.00%)",
+    })
+    public readonly taxRateBps: number;
+
     public constructor(entity: Location) {
         this.id = entity.id;
         this.name = entity.name;
         this.timezone = entity.timezone;
         this.currencyCode = entity.currencyCode;
+        this.taxRateBps = entity.taxRateBps ?? 0;
     }
 }
